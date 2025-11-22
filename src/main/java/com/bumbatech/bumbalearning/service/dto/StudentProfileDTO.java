f0a852294b928f832aa7e0339dc7250d0035fc2a
arcanjo.rodrigues <arcanjo.rodrigues@sicoob.com.br>
2025-11-22 19:32:19 -0300

    [FEAT] Perfil do Aluno (Frontend) implementado

--- src/main/java/com/bumbatech/bumbalearning/service/dto/StudentProfileDTO.java
+++ src/main/java/com/bumbatech/bumbalearning/service/dto/StudentProfileDTO.java
@@ -3,7 +3,11 @@ package com.bumbatech.bumbalearning.service.dto;
 import com.bumbatech.bumbalearning.domain.enumeration.UserLevel;
 import java.time.LocalDate;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class StudentProfileDTO {
 
     private Long userId;
@@ -20,6 +24,8 @@ public class StudentProfileDTO {
     private ProfileStatistics statistics;
     private List<RecentActivity> recentActivities;
 
+    @Setter
+    @Getter
     public static class ProfileStatistics {
 
         private Integer totalLessonsCompleted;
@@ -49,64 +55,10 @@ public class StudentProfileDTO {
             this.totalDaysActive = totalDaysActive;
             this.longestStreak = longestStreak;
         }
-
-        public Integer getTotalLessonsCompleted() {
-            return totalLessonsCompleted;
-        }
-
-        public void setTotalLessonsCompleted(Integer totalLessonsCompleted) {
-            this.totalLessonsCompleted = totalLessonsCompleted;
-        }
-
-        public Integer getTotalQuestionsAnswered() {
-            return totalQuestionsAnswered;
-        }
-
-        public void setTotalQuestionsAnswered(Integer totalQuestionsAnswered) {
-            this.totalQuestionsAnswered = totalQuestionsAnswered;
-        }
-
-        public Integer getTotalCorrectAnswers() {
-            return totalCorrectAnswers;
-        }
-
-        public void setTotalCorrectAnswers(Integer totalCorrectAnswers) {
-            this.totalCorrectAnswers = totalCorrectAnswers;
-        }
-
-        public Double getAccuracyRate() {
-            return accuracyRate;
-        }
-
-        public void setAccuracyRate(Double accuracyRate) {
-            this.accuracyRate = accuracyRate;
-        }
-
-        public Integer getTotalAchievementsUnlocked() {
-            return totalAchievementsUnlocked;
-        }
-
-        public void setTotalAchievementsUnlocked(Integer totalAchievementsUnlocked) {
-            this.totalAchievementsUnlocked = totalAchievementsUnlocked;
-        }
-
-        public Integer getTotalDaysActive() {
-            return totalDaysActive;
-        }
-
-        public void setTotalDaysActive(Integer totalDaysActive) {
-            this.totalDaysActive = totalDaysActive;
-        }
-
-        public Integer getLongestStreak() {
-            return longestStreak;
-        }
-
-        public void setLongestStreak(Integer longestStreak) {
-            this.longestStreak = longestStreak;
-        }
     }
 
+    @Setter
+    @Getter
     public static class RecentActivity {
 
         private String type;
@@ -122,141 +74,5 @@ public class StudentProfileDTO {
             this.date = date;
             this.xpEarned = xpEarned;
         }
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public LocalDate getDate() {
-            return date;
-        }
-
-        public void setDate(LocalDate date) {
-            this.date = date;
-        }
-
-        public Integer getXpEarned() {
-            return xpEarned;
-        }
-
-        public void setXpEarned(Integer xpEarned) {
-            this.xpEarned = xpEarned;
-        }
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMunicipalityCode() {
-        return municipalityCode;
-    }
-
-    public void setMunicipalityCode(String municipalityCode) {
-        this.municipalityCode = municipalityCode;
-    }
-
-    public UserLevel getCurrentLevel() {
-        return currentLevel;
-    }
-
-    public void setCurrentLevel(UserLevel currentLevel) {
-        this.currentLevel = currentLevel;
-    }
-
-    public Integer getTotalXp() {
-        return totalXp;
-    }
-
-    public void setTotalXp(Integer totalXp) {
-        this.totalXp = totalXp;
-    }
-
-    public Integer getCurrentStreak() {
-        return currentStreak;
-    }
-
-    public void setCurrentStreak(Integer currentStreak) {
-        this.currentStreak = currentStreak;
-    }
-
-    public Integer getDailyGoalXp() {
-        return dailyGoalXp;
-    }
-
-    public void setDailyGoalXp(Integer dailyGoalXp) {
-        this.dailyGoalXp = dailyGoalXp;
-    }
-
-    public LocalDate getLastActivityDate() {
-        return lastActivityDate;
-    }
-
-    public void setLastActivityDate(LocalDate lastActivityDate) {
-        this.lastActivityDate = lastActivityDate;
-    }
-
-    public ProfileStatistics getStatistics() {
-        return statistics;
-    }
-
-    public void setStatistics(ProfileStatistics statistics) {
-        this.statistics = statistics;
-    }
-
-    public List<RecentActivity> getRecentActivities() {
-        return recentActivities;
-    }
-
-    public void setRecentActivities(List<RecentActivity> recentActivities) {
-        this.recentActivities = recentActivities;
     }
 }
